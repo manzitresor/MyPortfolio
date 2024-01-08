4a040582ae0d2c61071f17e99e6e4abf12acafcf
manzitresor <83731517+manzitresor@users.noreply.github.com>
2024-01-08 21:21:00 +0200

    Add data file

---
+++ src/data/data.js
@@ -0,0 +1,95 @@
+import bookstore from "../assets/bookstore.png"
+import driveshare from "../assets/driveshare.JPG"
+import magician from "../assets/Capture.JPG"
+import badget from "../assets/badget.JPG"
+import reacticon from "../assets/react.png";
+import rails from "../assets/rails.png";
+import github from "../assets/rails.png";
+import javascript from "../assets/javascript.png";
+import Tailwind from "../assets/tailwind.png";
+import Mongo from "../assets/mongo.png";
+import Node from "../assets/github.png";
+
+export const projects = [
+    {
+      title: "Bookstore App",
+      subtitle: "React Application",
+      description:
+        "Book Store is a dynamic and user-friendly website designed to showcase an extensive collection of books. With its intuitive interface User can add and remove books from that list.",
+      image: bookstore,
+      demo: "https://bookstore-qbpn.onrender.com",
+      source: "https://github.com/manzitresor/book_store",
+    },
+    {
+      title: "Drive Share",
+      subtitle: "React and Rails Application",
+      description:
+        "DriveShare is a Full-stack web application. that makes easier to Add, reserve and delete cars.DriveShare is a Full-stack web application. that makes easier to Add, reserve and delete cars.",
+      image: driveshare,
+      demo: "https://drive-share-app-frontend.onrender.com/",
+      source: "https://github.com/Zohra-Neda/DriveShare-Front-End",
+    },
+    {
+      title: "Math Magicians",
+      subtitle: "Javascript Application",
+      description:
+        "Math magicians is a website for all fans of mathematics. It is a Single Page App (SPA) that allows users to Make simple calculations.",
+      image: magician,
+      demo: "https://mathmagician-ln19.onrender.com",
+      source: "https://github.com/manzitresor/math_magicians",
+    },
+    {
+      title: "Budget App",
+      subtitle: "React Hooks",
+      description:
+        "Budget App is a web application crafted with Ruby on Rails. With intuitive features helps users track their income and expenses.",
+      image: badget,
+      demo: "https://budget-app-u4o6.onrender.com",
+      source: "https://github.com/manzitresor/budget_app",
+    },
+  ];
+
+  
+
+  export const  skills = [
+    {
+      id: 1,
+      skill: "React",
+      icon: reacticon
+    },
+    {
+      id: 2,
+      skill: "Rails",
+      icon: rails
+    },
+    {
+      id: 3,
+      skill: "Github",
+      icon: github
+    },
+    {
+      id: 4,
+      skill: "Javascript",
+      icon: javascript
+    },
+    {
+      id: 5,
+      skill: "Javascript",
+      icon: javascript
+    },
+    {
+      id: 6,
+      skill: "Tailwind",
+      icon: Tailwind
+    },
+    {
+      id: 7,
+      skill: "Mongo",
+      icon: Mongo
+    },
+    {
+      id: 8,
+      skill: "Node js",
+      icon: Node
+    } 
+  ]
