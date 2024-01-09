d8ddec9e36cd191e3cb4383aa04291354d8ea6a8
manzitresor <83731517+manzitresor@users.noreply.github.com>
2024-01-09 21:12:01 +0200

    Add mail to

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Contact from "./components/Contact"
 function App() {
   return (
     <>
-        <main className='text-gray-400 bg-gray-900 body-font'>
+      <main className='text-gray-400 bg-gray-900 body-font'>
       <Navbar/>
       <About/>
       <Skills/>
